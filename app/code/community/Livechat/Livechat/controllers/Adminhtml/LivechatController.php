cbba744ae1a7872300fedee149422cda72b60235
Manuele Menozzi <manuele.menozzi@gmail.com>
2018-12-20 08:41:23 +0100

    Bumps version 1.1.8

--- app/code/community/Livechat/Livechat/controllers/Adminhtml/LivechatController.php
+++ app/code/community/Livechat/Livechat/controllers/Adminhtml/LivechatController.php
@@ -20,19 +20,16 @@ class Livechat_Livechat_Adminhtml_LivechatController extends Mage_Adminhtml_Cont
 		$livechat_license_number = filter_input(INPUT_POST, 'livechat_license_number',FILTER_SANITIZE_SPECIAL_CHARS);
 		$livechat_login = filter_input(INPUT_POST, 'livechat_login',FILTER_SANITIZE_SPECIAL_CHARS);
 		$livechat_mobile = filter_input(INPUT_POST, 'livechat_mobile',FILTER_SANITIZE_SPECIAL_CHARS);
-		$livechat_sounds = filter_input(INPUT_POST, 'livechat_sounds',FILTER_SANITIZE_SPECIAL_CHARS);
 		$livechat_cart = filter_input(INPUT_POST, 'livechat_cart',FILTER_SANITIZE_SPECIAL_CHARS);
 
 		!isset($livechat_license_number) ? $livechat_license_number = ' ' : $livechat_license_number;
 		!isset($livechat_login) ? $livechat_login = ' ' : $livechat_login;
 		!isset($livechat_mobile) ? $livechat_mobile = 'No' : $livechat_mobile;  
-		!isset($livechat_sounds) ? $livechat_sounds = 'No' : $livechat_sounds;  
 		!isset($livechat_cart) ? $livechat_cart = 'Yes' : $livechat_cart;  
 
 		Mage::getConfig()->saveConfig('livechat/general/license', $livechat_license_number);
 		Mage::getConfig()->saveConfig('livechat/general/login', $livechat_login);
 		Mage::getConfig()->saveConfig('livechat/general/mobile', $livechat_mobile);
-		Mage::getConfig()->saveConfig('livechat/general/sounds', $livechat_sounds);
 		Mage::getConfig()->saveConfig('livechat/general/cart', $livechat_cart);
 
 		Mage::app()->getCacheInstance()->flush();
@@ -44,7 +41,6 @@ class Livechat_Livechat_Adminhtml_LivechatController extends Mage_Adminhtml_Cont
 		Mage::getConfig()->deleteConfig('livechat/general/license');
 		Mage::getConfig()->deleteConfig('livechat/general/login');
 		Mage::getConfig()->deleteConfig('livechat/general/mobile');
-		Mage::getConfig()->deleteConfig('livechat/general/sounds');
 		Mage::getConfig()->deleteConfig('livechat/general/cart');
 
 		Mage::app()->getCacheInstance()->flush();
--- readme.md
+++ readme.md
@@ -1,3 +1,14 @@
-# livechat-livechat
+# Magento 1.x plugin
 
-Mirror of Livechat_Livechat Magento extension.
+To prepare *.tgz file for this integration please use [MagentoTarToConnect](https://github.com/astorm/MagentoTarToConnect) tool.
+It will make creating archive as painless as possible.
+
+To use aforementioned tool you need config file. In main directory there is `config.php` file that is mostly ready just fill two parameters: `path_output`, `base_dir`, and update third - `archive_files`.
+After that, just do 
+```php
+$ ./magento-tar-to-connect.php config.php
+```
+and upload newly created `*.tgz`.
+
+
+
